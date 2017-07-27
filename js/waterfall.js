91790c835cae295e06247608d841d17a1713b577
frank198978104 <frank198978104@gmail.com>
2017-07-27 17:44:10 +0800

    commit

--- js/waterfall.js
+++ js/waterfall.js
@@ -14,124 +14,27 @@
      */
   var demoContent = [
     {
+      demo_link: 'http://chanceat.azurewebsites.net/Login/Index',
+      img_link: 'http://chanceat.azurewebsites.net/Content/img/logo.png',
+      title: '晨食餐飲集團',
+      core_tech: 'ASP.NET MVC',
+      description: '資訊工業策進會結訓專題'
+    },
+
+    {
       demo_link: 'https://codepen.io/haoyang/pen/jrvrQq',
-      img_link: 'https://ooo.0o0.ooo/2016/11/24/5836d81f48cd2.png',
-      code_link: 'https://codepen.io/haoyang/pen/jrvrQq',
-      title: 'Fisher-Yates 洗牌算法動畫',
-      core_tech: 'JavaScript',
-      description: 'Fisher-Yates 洗牌算法動畫。算法詳情見 <a href ="https://gaohaoyang.github.io/2016/10/16/shuffle-algorithm/">這裡</a>。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/test/headerTransition/',
-      img_link: 'https://ooo.0o0.ooo/2016/06/20/5768c1597d1fe.png',
-      code_link: 'https://github.com/Gaohaoyang/test/tree/master/headerTransition',
-      title: 'Header Transition 漸變動畫',
-      core_tech: 'jQuery BootStrap CSS3',
-      description: '花費不到半小時幫師兄做了一個簡單的 CSS3 動畫效果，當頁面滾動到指定距離時，header 區的背景色由透明變為藍色。仿照了網站 <a href ="https://quorrajs.org/">https://quorrajs.org/</a> 的 Header 區動畫效果。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/mask-fade-out/',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/demo-fade-out.png',
-      code_link: 'https://github.com/Gaohaoyang/mask-fade-out',
-      title: '遮罩層按指定路徑縮小消失',
-      core_tech: 'jQuery CSS',
-      description: '使用 animate 方法，做到兼容 IE8。曾在聯想服務官網上線3個月。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ToDo-WebApp/',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/blog-todoWebApp.png',
-      code_link: 'https://github.com/Gaohaoyang/ToDo-WebApp',
-      title: '百度前端學院 task0004 ToDo 應用(移動端)',
-      core_tech: 'JavaScript LocalStorage requireJS Sass Gulp XSS',
-      description: '在任務三中，做了一個 PC 端的 ToDo 應用。任務四是將它優化，以適應移動端設備。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/baidu-ife-practice/task0003/',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/demo-todo.png',
-      code_link: 'https://github.com/Gaohaoyang/baidu-ife-practice/tree/master/task0003',
-      title: '百度前端學院 task0003 ToDo 應用',
-      core_tech: 'JavaScript LocalStorage',
-      description: '任務三，ToDo 單頁應用，主要使用了 LocalStorage 存儲數據，使用 JSON 模擬了 3 張數據表。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0002/work/Gaohaoyang/task0002_5.html',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/demo-drag.png',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0002/work/Gaohaoyang',
-      title: '拖拽交互',
-      core_tech: 'JavaScript',
-      description: '對鼠標事件應用，以及判斷定位的方法等。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0002/work/Gaohaoyang/task0002_4.html',
-      img_link: 'http://ww2.sinaimg.cn/large/7011d6cfjw1f3ba2krzs0j207005y0sv.jpg',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0002/work/Gaohaoyang',
-      title: '輸入框即時提示',
-      core_tech: 'JavaScript',
-      description: '對input監聽，使用正在表達式高亮匹配，實現輸入聯想效果。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0002/work/Gaohaoyang/task0002_3.html',
-      img_link: 'http://ww2.sinaimg.cn/large/7011d6cfjw1f3ba04okoqj20eq093wh1.jpg',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0002/work/Gaohaoyang',
-      title: '輪播圖',
-      core_tech: 'JavaScript',
-      description: '變速運動，運動終止條件的應用。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0002/work/Gaohaoyang/task0002_2.html',
-      img_link: 'http://ww4.sinaimg.cn/large/7011d6cfjw1f3b9w6xpz5j20ae02pgm3.jpg',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0002/work/Gaohaoyang',
-      title: '倒計時',
-      core_tech: 'JavaScript',
-      description: 'setInterval()，Date 對象的學習和使用。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0002/work/Gaohaoyang/task0002_1.html',
-      img_link: 'http://ww3.sinaimg.cn/large/7011d6cfjw1f3b9tmyuh2j20au0aaaar.jpg',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0002/work/Gaohaoyang',
-      title: '處理興趣愛好列表',
-      core_tech: 'JavaScript',
-      description: '對JavaScript正則表達式和字符串的練習。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0002/work/Gaohaoyang/index.html',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/demo-demo-index.png',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0002/work/Gaohaoyang',
-      title: '百度前端學院 task0002 展示主頁',
-      core_tech: 'HTML CSS',
-      description: '任務二的展示主頁，裡麵包含五個小 demo。還有一篇相關的<a href="http://gaohaoyang.github.io/2015/04/22/baidu-ife-2-javascript/" target="_blank">日誌。</a>'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ife/task/task0001/work/Gaohaoyang/index.html',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/Demo-blog-ife.jpg',
-      code_link: 'https://github.com/Gaohaoyang/ife/tree/master/task/task0001/work/Gaohaoyang',
-      title: '百度前端學院 task0001 個人博客',
-      core_tech: 'HTML CSS',
-      description: '完成百度前端學院的任務。深刻的理解了BFC、浮動、inline-block間距，多列布局等技術。還有一篇相關的<a href="http://gaohaoyang.github.io/2015/04/15/baidu-ife-1/" target="_blank">日誌。</a>'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/ghost-button-css3/',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/DemoGhost-Button.png',
-      code_link: 'https://github.com/Gaohaoyang/ghost-button-css3',
-      title: 'Ghost Button 幽靈按鈕',
-      core_tech: 'CSS3',
-      description: '使用 CSS3 中的旋轉、縮放、過渡、動畫等效果，製作出很酷的按鈕效果。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/shadow-demo-css3',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/Demoshadow.png',
-      code_link: 'https://github.com/Gaohaoyang/shadow-demo-css3',
-      title: 'CSS3 陰影特效',
-      core_tech: 'CSS3',
-      description: 'CSS3 中的陰影、旋轉等效果，製作出曲邊陰影和翹邊陰影。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/learning-AngularJS/2-3-bookstore-add-sth-by-myself/',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/DemoAngularJS.png',
-      code_link: 'https://github.com/Gaohaoyang/learning-AngularJS/tree/master/2-3-bookstore-add-sth-by-myself',
-      title: 'AngularJS 結合動畫效果',
-      core_tech: 'AngularJS CSS3',
-      description: '使用 AngularJS 中的 ngAnimate 加 CSS3里面的一些旋轉效果，做出頁面切換的效果。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/learning-AngularJS/2-4-UiRouterPractice',
-      img_link: 'http://ww2.sinaimg.cn/large/7011d6cfjw1f3b8zumfqij20q40nh76x.jpg',
-      code_link: 'https://github.com/Gaohaoyang/learning-AngularJS/tree/master/2-4-UiRouterPractice',
-      title: 'AngularJS UI-router 練習',
-      core_tech: 'AngularJS UI-router',
-      description: 'UI-router 作為 AngularJS 中路由的擴充，實現了多級路由的效果。使得前端界面跳轉更加方便。'
-    }, {
-      demo_link: 'http://gaohaoyang.github.io/test/bootstrap-zhihu/',
-      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/teach-girlfriend-html-CopyZhihu.jpg',
-      code_link: 'https://github.com/Gaohaoyang/test/tree/master/bootstrap-zhihu',
-      title: '仿知乎頁面',
-      core_tech: 'HTML BootStrap',
-      description: '使用BootStrap仿照知乎做了一個靜態頁面。'
+      img_link: 'https://tad.tycg.gov.tw/images/bg_index_banner.png',
+      title: '桃園市政府交通事件裁決處',
+      core_tech: 'ASP.NET MVC',
+      description: '桃園市政府交通事件裁決處，線上申辦、便民服務'
+    },
+    
+    {
+      demo_link: 'http://app.creatidea.com.tw/EpaAnniversary',
+      img_link: 'http://app.creatidea.com.tw/EpaAnniversary/images/img_index_banner.jpg',
+      title: '環保署30週年慶',
+      core_tech: 'ASP.NET MVC',
+      description: '行政院環保署30週年慶，各局處大事記、各地今昔對比、多媒體圖片影音電子書。'
     }
   ];
 
@@ -162,7 +65,7 @@ function contentInit(content) {
   // var htmlStr = htmlArr.join('')
   var htmlStr = ''
   for (var i = 0; i < content.length; i++) {
-    htmlStr += '<div class="grid-item">' + '   <a class="a-img" href="' + content[i].demo_link + '">' + '       <img src="' + content[i].img_link + '">' + '   </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p>主要技術：' + content[i].core_tech + '</p>' + '   <p>' + content[i].description + '       <a href="' + content[i].code_link + '">源代碼 <i class="fa fa-code" aria-hidden="true"></i></a>' + '   </p>' + '</div>'
+    htmlStr += '<div class="grid-item">' + '   <a class="a-img" href="' + content[i].demo_link + '">' + '       <img src="' + content[i].img_link + '">' + '   </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p>主要技術：' + content[i].core_tech + '</p>' + '   <p>' + content[i].description + '       <a href="' + content[i].code_link + '"></a>' + '   </p>' + '</div>'
   }
   var grid = document.querySelector('.grid')
   grid.insertAdjacentHTML('afterbegin', htmlStr)
--- page/4about.md
+++ page/4about.md
@@ -5,97 +5,29 @@ permalink: /about/
 icon: heart
 type: page
 ---
-
+<div align="center">
+    <img src="https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/13445548_1334825446531034_534270302915026847_n.jpg?oh=819006d7ed24d5f7cc0142d405a24fce&oe=59FD35D8" height="250">
+</div>
 * content
 {:toc}
 
-## 關於我
+## Author
 
-<iframe src="https://githubbadge.appspot.com/gaohaoyang?s=1" style="border: 0;height: 142px;width: 200px;overflow: hidden;" frameBorder="0"></iframe>
+In 2012, I graduated from Chung Hua University where I majored in Computer Science and Information Engineering.
 
-就職於阿里巴巴集團天貓事業部，前端開發工程師，花名川軾。
+I've been working for CREATIDEA Company as a ASP.NET Developer.
 
-主要興趣集中在前端開發，大前端的概念深入我心。將更多的業務邏輯放在前端中，這很有趣。同時，將自己構想的內容實現出來，這種感覺很愉快。
+In my free time, I like singing a song, playing guitar, and listening to music.
 
-熱愛美好的事物，熱愛搖滾樂，組過樂隊，賣過唱。
+## Skill Set
 
-很喜歡崔健的歌詞：現實像塊石頭，精神像個蛋，石頭雖然堅硬，可蛋裡才是生命。
+* C/C++, Visual C#
+* XML, JSON
+* HTML5, CSS3, JavaScript, jQuery
+* ASP.NET, ASP.NET MVC, LINQ
+* IIS, SQL Server2014, EntityFramework
 
-還有，學習是一個非常愉快的過程。共同加油！
+## Contact Me
 
-* 2016.6.30 入職阿里巴巴天貓事業部
-* 2016.6 畢業於西安交通大學 軟件工程碩士
-* 2015.12 獲得2014-2015學年二等獎學金
-* 2015.7-2015.8 聯想服務，前端開發工程師（實習生）
-* 2013.6 畢業於西安交通大學 理學院
-* 2001 獲得西航四校總成績年級排名第八名
-
-## 聯繫我
-
-* GitHub：[Gaohaoyang](https://github.com/Gaohaoyang)
-* email：gaohaoyang126@126.com
-* [Weibo](http://weibo.com/3115521wh)
-* [知乎](https://www.zhihu.com/people/gaohaoyang)
-* [Facebook](https://www.facebook.com/gaohaoyang.water)
-* [Twitter](https://twitter.com/gaohaoyang126)
-* [豆瓣](https://www.douban.com/people/42525035/)
-* [豆瓣音樂人-浩陽的小站](https://site.douban.com/haoyangaiyinyue/)
-
-## 關於本站
-
-**若您想使用這個 jekyll 博客主題，請訪問：[https://github.com/Gaohaoyang/gaohaoyang.github.io](https://github.com/Gaohaoyang/gaohaoyang.github.io)**
-
-~歡迎使用~
-
-歡迎大家提一些對本站主題的建議，可以直接在下面留言或者新建一個 [Issue](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues)。
-
-### Update Log
-
-*2017.2.28*
-
-- `[^]` 修復目錄滾動 bug [#22](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues/22), [#48](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues/48)
-
-*2016.6.20*
-
-* `[+]` 在文章頁中添加上一篇和下一篇文章鏈接。
-* `[^]` 修改 font-family 順序，避免微軟雅黑將單引號解析為全角。
-* `[^]` 修復標籤雲算法中被除數為零的 bug。[#26](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues/26), [#28](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues/28), [#30](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues/30)
-
-*2016.5.11 v2.0.1*
-
-* `[^]` 優化代碼，將頁面中的大段評論相關代碼抽離出來，放入`comments.html`
-* `[+]` 添加百度統計和Google分析代碼，在`_config.yml`中配置相關參數即可
-* `[+]` 更新文檔，添加博客主題使用方法，便於上手
-* `[+]` 添加了`favicon.ico`
-* `[^]` 修復 bug，目錄太長時，滾動到最底部時隱藏到footer下面。修復後長目錄在滾動到底部時使用`position:absolute`
-* `[^]` 修改目錄區的滾動條樣式（僅針對`webkit`內核瀏覽器）
-* `[^]` 修改 demo 頁中 disqus 評論區 a 標籤的顏色 bug，修改 blockqoute 中 p 標籤的 margin
-* `[+]` 添加不蒜子計數功能，在footer上顯示訪問量
-* `[+]` 添加回到頂部功能
-
-*2016.4.27 v2.0.0*
-
-* `[^]` 基於 jekyll 3.1.2 重構了所有代碼
-* `[+]` 主頁添加了摘要，在正文中使用4個換行符來分割，可在`_config.yml`中修改
-* `[+]` 主頁添加了近期文章、分類列表和標籤雲
-* `[+]` 主頁導航區做了視覺優化，陰影效果
-* `[+]` 增加了歸檔、標籤和分類頁面
-* `[+]` 增加了收藏頁面
-* `[+]` 評論插件可以選擇 disqus 或 多說，直接在`_config.yml`中修改
-* `[+]` 適配移動端
-* `[+]` 頁面滾動時，文章目錄固定在右側
-* `[+]` 頁面內容較少時，固定 footer 在頁面底部
-* `[^]` 使用 GitHub 風格的代碼高亮寫法，即\`\`\`的寫法，去除`highlight.js`代碼高亮插件的使用
-* `[^]` 使用 Masonry 重寫了 Demo 頁中的瀑布流布局，響應式交互體驗更好
-* `[-]` 去除了 jQuery 和 BootStrap，使得加載速度更快
-
-* 2016.3-2016.4 進行了一次大的改版和重構，詳見 [README](https://github.com/Gaohaoyang/gaohaoyang.github.io/blob/master/README.md) 和博文 [對這個 jekyll 博客主題的改版和重構](http://gaohaoyang.github.io/2016/03/12/jekyll-theme-version-2.0/)
-* 2015.3-2015.4 完成了這個博客主題的第一版。
-
-## 友情鏈接
-
-[羡轍雜俎](http://zhangwenli.com/blog) \| [Anotherhome](https://www.anotherhome.net) \| [Reverland](http://reverland.org/) \| [ZhiLi](http://lizhipower.github.io/) \| [Simmer](http://simmer-jun.github.io/) \| [awthink](http://awthink.net/) \| [Aralic](http://aralic.github.io/) \| [zchen9](http://www.chen9.info/) \| [wuhuaji](http://wuhuaji.me/) \| [lisheng](http://www.lishengcn.cn/) \| [薛彬XueBin](http://axuebin.com/blog/) \| [TBOOX](http://www.tboox.org/cn/) \|  [Ling](http://linglinyp.com/)
-
-## Comments
-
-{% include comments.html %}
+* [Github](https://github.com/frank198978104)
+* [LinkedIn](https://www.linkedin.com/in/frank-sun-ab2472139/)
